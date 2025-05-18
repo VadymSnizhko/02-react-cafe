bdb6ca325d70e6e8fd1092cfcc70f6c128550464
VadymSnizhko <snizhko@gmail.com>
2025-05-19 00:04:33 +0300

    task 01 step 5-8 Home Work 02 React

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import css from './App.module.css'
 import CafeInfo from '../CafeInfo/Cafeinfo'
 import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
+
 import type { Votes, VoteType } from '../../types/votes';
 
 function App() {
@@ -29,12 +32,29 @@ function App() {
     });
   };
 
-  return (
-    <div className={css.app}>
-      <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
-    </div>
-  );
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+
+    return (
+      <div className={css.app}>
+        <CafeInfo />
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
+
+        {totalVotes > 0 ? (
+          <VoteStats
+            votes={votes}
+            totalVotes={totalVotes}
+            positiveRate={positiveRate}
+          />
+        ) : (
+          <Notification />
+        )}
+      </div>
+    );
+    
 }
 
 export default App;
